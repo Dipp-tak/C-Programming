d28aa7158828b1879783e365183c85c2f5459483
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2023-12-07 01:29:19 +0530

    Create some of odd number upto n .c

---
+++ loop/while-loop/some of odd number upto n .c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i=1,n,sum=0;
+	printf("enter the value of n:");
+	scanf("%d",&n);
+	
+	while(i<=n)
+	{
+		if(i%2!=0)
+		{
+			sum = sum+ i;
+		}
+		i++;
+	}
+	printf("%d\n",sum); 
+	return 0;
+}
